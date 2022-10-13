a538e9c75765b927e534680c50d5b104192103e7
Luca Danelutti <lucadanelutti@outlook.com>
2022-10-13 16:54:01 +0200

    Add node jsonpath async readline implementation

--- benchmark/nodejsonpath/nodejsonpath_async.js
+++ benchmark/nodejsonpath/nodejsonpath_async.js
@@ -1,17 +1,17 @@
 const jp = require("jsonpath");
 const fs = require("fs");
+const readline = require('readline');
 const { performance } = require('perf_hooks');
 
 const base_dir = "../../../datasets/";
 
 // $.user.lang
-function query_TT1(dataset) {
-    var file = fs.readFileSync(base_dir + dataset, `utf-8`).toString().split("\n");
-
+async function query_TT1(dataset) {
+    var lineReader = readline.createInterface({input: fs.createReadStream(base_dir + dataset)});
     var count = 0;
-    for (var i = 0; i < file.length; i++) {
+    for await (const line of lineReader) {
         try {
-            const obj = JSON.parse(file[i]);
+            const obj = JSON.parse(line);
             let value = jp.query(obj, '$.user.lang');
             count += value.length;
         } catch (error) {
@@ -22,13 +22,13 @@ function query_TT1(dataset) {
 }
 
 // {$.user.lang, $.lang}
-function query_TT2(dataset) {
-    var file = fs.readFileSync(base_dir + dataset, `utf-8`).toString().split("\n");
+async function query_TT2(dataset) {
+    var lineReader = readline.createInterface({input: fs.createReadStream(base_dir + dataset)});
 
     var count = 0;
-    for (var i = 0; i < file.length; i++) {
+    for await (const line of lineReader) {
         try {
-            const obj = JSON.parse(file[i]);
+            const obj = JSON.parse(line);
             try {
                 let value = jp.query(obj, '$.user.lang');
                 count += value.length;
@@ -47,13 +47,13 @@ function query_TT2(dataset) {
 }
 
 // $.user.lang[?(@ == 'nl')]"
-function query_TT3(dataset) {
-    var file = fs.readFileSync(base_dir + dataset, `utf-8`).toString().split("\n");
+async function query_TT3(dataset) {
+    var lineReader = readline.createInterface({input: fs.createReadStream(base_dir + dataset)});
 
     var count = 0;
-    for (var i = 0; i < file.length; i++) {
+    for await (const line of lineReader) {
         try {
-            const obj = JSON.parse(file[i]);
+            const obj = JSON.parse(line);
             let value = jp.query(obj, '$.user.lang');
             if (value == "nl")
                 count += value.length;
@@ -65,13 +65,13 @@ function query_TT3(dataset) {
 }
 
 // $.user.lang[?(@ == 'en')]"
-function query_TT4(dataset) {
-    var file = fs.readFileSync(base_dir + dataset, `utf-8`).toString().split("\n");
+async function query_TT4(dataset) {
+    var lineReader = readline.createInterface({input: fs.createReadStream(base_dir + dataset)});
 
     var count = 0;
-    for (var i = 0; i < file.length; i++) {
+    for await (const line of lineReader) {
         try {
-            const obj = JSON.parse(file[i]);
+            const obj = JSON.parse(line);
             let value = jp.query(obj, '$.user.lang');
             if (value == "en")
                 count += value.length;
@@ -83,13 +83,13 @@ function query_TT4(dataset) {
 }
 
 // {$.bestMarketplacePrice.price, $.name}
-function query_WM(dataset) {
-    var file = fs.readFileSync(base_dir + dataset, `utf-8`).toString().split("\n");
+async function query_WM(dataset) {
+    var lineReader = readline.createInterface({input: fs.createReadStream(base_dir + dataset)});
 
     var count = 0;
-    for (var i = 0; i < file.length; i++) {
+    for await (const line of lineReader) {
         try {
-            const obj = JSON.parse(file[i]);
+            const obj = JSON.parse(line);
             try {
                 let value = jp.query(obj, '$.bestMarketplacePrice.price');
                 count += value.length;
@@ -108,13 +108,13 @@ function query_WM(dataset) {
 }
 
 // $.categoryPath[1:3].id
-function query_BB(dataset) {
-    var file = fs.readFileSync(base_dir + dataset, `utf-8`).toString().split("\n");
+async function query_BB(dataset) {
+    var lineReader = readline.createInterface({input: fs.createReadStream(base_dir + dataset)});
 
     var count = 0;
-    for (var i = 0; i < file.length; i++) {
+    for await (const line of lineReader) {
         try {
-            const obj = JSON.parse(file[i]);
+            const obj = JSON.parse(line);
             try {
                 let value = jp.query(obj, '$.categoryPath[1:3].id');
                 count += value.length;
@@ -126,19 +126,20 @@ function query_BB(dataset) {
     return count;
 }
 
-function execute(warmup_query, repeat_query, dataset, func, query) {
+
+async function execute(warmup_query, repeat_query, dataset, func, query) {
     console.log("Starting warmup queries on dataset " + dataset);
     var num_results;
     for (let i = 0; i < warmup_query; i++)
-        num_results = func(dataset);
+        num_results = await func(dataset);
     const start = performance.now();
     for (let i = 0; i < repeat_query; i++)
-        func(dataset);
+        await func(dataset);
     const delay = performance.now() - start;
     console.log("Executed query " + query + " on dataset " + dataset + " in " + delay / repeat_query + "ms; results: " + num_results);
 }
 
-function app() {
+async function app() {
     const warmup_query = 1;
     const repeat_query = 1;
 
@@ -146,14 +147,14 @@ function app() {
     const walmart_small = "walmart_small_records.json";
     const bestbuy_small = "bestbuy_small_records.json";
 
-    execute(warmup_query, repeat_query, twitter_small, query_TT1, "TT1");
-    execute(warmup_query, repeat_query, twitter_small, query_TT2, "TT2");
-    execute(warmup_query, repeat_query, twitter_small, query_TT3, "TT3");
-    execute(warmup_query, repeat_query, twitter_small, query_TT4, "TT4");
+    await execute(warmup_query, repeat_query, twitter_small, query_TT1, "TT1");
+    await execute(warmup_query, repeat_query, twitter_small, query_TT2, "TT2");
+    await execute(warmup_query, repeat_query, twitter_small, query_TT3, "TT3");
+    await execute(warmup_query, repeat_query, twitter_small, query_TT4, "TT4");
 
-    execute(warmup_query, repeat_query, walmart_small, query_WM, "WM");
+    await execute(warmup_query, repeat_query, walmart_small, query_WM, "WM");
 
-    execute(warmup_query, repeat_query, bestbuy_small, query_BB, "BB");
+    await execute(warmup_query, repeat_query, bestbuy_small, query_BB, "BB");
 }
 
 app();
