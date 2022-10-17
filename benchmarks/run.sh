371cfee1126fc07af1f42bf6c99e56b0389b0f50
Luca Danelutti <lucadanelutti@outlook.com>
2022-10-17 15:35:01 +0200

    Add parametric warmup and repeat params

--- benchmarks/run.sh
+++ benchmarks/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 NODE_PATH="$HOME/node-v16.18.0/bin"
+WARMUP=1
+REPEAT=1
 
 echo "Starting benchmarks"
 echo "engine,dataset,query,time,results,warmup,repeat" > results.csv
@@ -8,62 +10,62 @@ echo "engine,dataset,query,time,results,warmup,repeat" > results.csv
 # gpjson
 echo "running gpjson"
 cd gpjson
-node --jvm --polyglot gpsjon.js >> ../results.csv
+node --jvm --polyglot gpsjon.js warmup=$WARMUP repeat=$REPEAT >> ../results.csv
 cd ..
 
 # javajsonpath
 echo "running javajsonpath"
 cd javajsonpath
 mvn package
-java -jar target/javajsonpath-1.0-SNAPSHOT-jar-with-dependencies.jar >> ../results.csv
+java -jar target/javajsonpath-1.0-SNAPSHOT-jar-with-dependencies.jar warmup=$WARMUP repeat=$REPEAT >> ../results.csv
 cd ..
 
 # nodejsonpath
 echo "running nodejsonpath"
 cd nodejsonpath
 $NODE_PATH/npm install jsonpath
-$NODE_PATH/node async_nodejsonpath.js >> ../results.csv
+$NODE_PATH/node async_nodejsonpath.js warmup=$WARMUP repeat=$REPEAT >> ../results.csv
 cd ..
 
 # nodejsonpathplus
 echo "running nodejsonpathplus"
 cd nodejsonpathplus
 $NODE_PATH/npm install jsonpath-plus
-$NODE_PATH/node async_nodejsonpathplus.js >> ../results.csv
+$NODE_PATH/node async_nodejsonpathplus.js warmup=$WARMUP repeat=$REPEAT >> ../results.csv
 cd ..
 
 # nodemanual
 echo "running nodemanual"
 cd nodemanual
-$NODE_PATH/node async_nodemanual.js >> ../results.csv
+$NODE_PATH/node async_nodemanual.js warmup=$WARMUP repeat=$REPEAT >> ../results.csv
 cd ..
 
 # nodesimdjson
 echo "running nodesimdjson"
 cd nodesimdjson
 $NODE_PATH/npm install simdjson
-$NODE_PATH/node async_nodesimdjson.js >> ../results.csv
+$NODE_PATH/node async_nodesimdjson.js warmup=$WARMUP repeat=$REPEAT >> ../results.csv
 cd ..
 
 # pison
 echo "running pison"
 cd pison
 make
-./bin/pison >> ../results.csv
+./bin/pison warmup=$WARMUP repeat=$REPEAT >> ../results.csv
 cd ..
 
 # rapidjson
 echo "running rapidjson"
 cd rapidjson
 make
-./bin/rapidjson >> ../results.csv
+./bin/rapidjson warmup=$WARMUP repeat=$REPEAT >> ../results.csv
 cd ..
 
 # simdjson
 echo "running simdjson"
 cd simdjson
 make
-./bin/simdjson >> ../results.csv
+./bin/simdjson warmup=$WARMUP repeat=$REPEAT >> ../results.csv
 cd ..
 
 echo "Benchmarks done"
