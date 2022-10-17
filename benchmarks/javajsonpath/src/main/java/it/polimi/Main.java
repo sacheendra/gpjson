1cdafc14a38eb7029456b358676aa4c338d4d070
Luca Danelutti <lucadanelutti@outlook.com>
2022-10-17 14:41:07 +0200

    Fix dataset print

--- benchmarks/javajsonpath/src/main/java/it/polimi/Main.java
+++ benchmarks/javajsonpath/src/main/java/it/polimi/Main.java
@@ -3,7 +3,7 @@ package it.polimi;
 public class Main {
     private final static int warmup_query = 5;
     private final static int repeat_query = 10;
-    private final static String base_dir = "../../../datasets/";
+    private final static String baseDir = "../../../datasets/";
 
     interface I {
         public int query(String dataset);
@@ -17,13 +17,13 @@ public class Main {
         String bestbuy_small = "bestbuy_small_records.json";
         String walmart_small = "walmart_small_records.json";
 
-        exe.execute(warmup_query, repeat_query, base_dir + twitter_small, Query::TT1, "TT1");
-        exe.execute(warmup_query, repeat_query, base_dir + twitter_small, Query::TT2, "TT2");
-        exe.execute(warmup_query, repeat_query, base_dir + twitter_small, Query::TT3, "TT3");
-        exe.execute(warmup_query, repeat_query, base_dir + twitter_small, Query::TT4, "TT4");
+        exe.execute(warmup_query, repeat_query, twitter_small, baseDir, Query::TT1, "TT1");
+        exe.execute(warmup_query, repeat_query, twitter_small, baseDir, Query::TT2, "TT2");
+        exe.execute(warmup_query, repeat_query, twitter_small, baseDir, Query::TT3, "TT3");
+        exe.execute(warmup_query, repeat_query, twitter_small, baseDir, Query::TT4, "TT4");
 
-        exe.execute(warmup_query, repeat_query, base_dir + walmart_small, Query::WM, "WM");
+        exe.execute(warmup_query, repeat_query, walmart_small, baseDir, Query::WM, "WM");
 
-        exe.execute(warmup_query, repeat_query, base_dir + bestbuy_small, Query::BB, "BB");
+        exe.execute(warmup_query, repeat_query, bestbuy_small, baseDir, Query::BB, "BB");
     }
 }
