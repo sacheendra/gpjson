947b4201ea4653c402f252ba2f2d3d3c8c04cd4d
Luca Danelutti <lucadanelutti@outlook.com>
2022-10-18 14:03:16 +0200

    Better commands handling

--- benchmarks/run_batch1.sh
+++ benchmarks/run_batch1.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# exit when any command fails
+set -e
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
+
 NODE_PATH="$HOME/node-v16.18.0/bin"
 WARMUP=1
 REPEAT=2
@@ -16,21 +24,21 @@ cd ..
 # javajsonpath
 echo "running javajsonpath"
 cd javajsonpath
-mvn package
+mvn package > /dev/null 2>&1
 java -cp target/javajsonpath-1.0-SNAPSHOT-jar-with-dependencies.jar it.polimi.Batch1 warmup=$WARMUP repeat=$REPEAT >> ../results.csv
 cd ..
 
 # nodejsonpath
 echo "running nodejsonpath"
 cd nodejsonpath
-$NODE_PATH/npm install jsonpath
+$NODE_PATH/npm install jsonpath > /dev/null 2>&1
 $NODE_PATH/node batch1.js warmup=$WARMUP repeat=$REPEAT >> ../results.csv
 cd ..
 
 # nodejsonpathplus
 echo "running nodejsonpathplus"
 cd nodejsonpathplus
-$NODE_PATH/npm install jsonpath-plus
+$NODE_PATH/npm install jsonpath-plus > /dev/null 2>&1
 $NODE_PATH/node batch1.js warmup=$WARMUP repeat=$REPEAT >> ../results.csv
 cd ..
 
@@ -43,28 +51,28 @@ cd ..
 # nodesimdjson
 echo "running nodesimdjson"
 cd nodesimdjson
-$NODE_PATH/npm install simdjson
+$NODE_PATH/npm install simdjson > /dev/null 2>&1
 $NODE_PATH/node batch1.js warmup=$WARMUP repeat=$REPEAT >> ../results.csv
 cd ..
 
 # pison
 echo "running pison"
 cd pison
-make batch1
+make batch1 > /dev/null 2>&1
 ./bin/batch1 warmup=$WARMUP repeat=$REPEAT >> ../results.csv
 cd ..
 
 # rapidjson
 echo "running rapidjson"
 cd rapidjson
-make batch1
+make batch1 > /dev/null 2>&1
 ./bin/batch1 warmup=$WARMUP repeat=$REPEAT >> ../results.csv
 cd ..
 
 # simdjson
 echo "running simdjson"
 cd simdjson
-make batch1
+make batch1 > /dev/null 2>&1
 ./bin/batch1 warmup=$WARMUP repeat=$REPEAT >> ../results.csv
 cd ..
 
